95a7dfb6fb51fcb9d0a7d0f4a17f8ba66d85fc15
Mashael S <mashaeelsaaud94@gmail.com>
2023-03-15 19:31:57 +0300

    Changed the theme & data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,38 +7,133 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
+        backgroundColor: Colors.purple[800],
         title: const Text(
-          'Home',
+          "Hello",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
         ),
-        backgroundColor: const Color.fromARGB(255, 55, 2, 201),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            width: 20,
-            height: 20,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.person,
+                      size: 40,
+                      color: Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Account",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.purple[500],
+                      ),
+                    ),
+                  ],
+                ),
+                const Divider(
+                  height: 20,
+                  thickness: 2,
+                  color: Colors.black26,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Personal Information",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Full Name: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "National ID: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Gender: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Location",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "City: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Home Address: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                Image.network(
+                  'https://i0.wp.com/linkedksa.com/wp-content/uploads/2022/06/29DEA9F2-F9A5-43BE-B22F-322F36F6B757.png?fit=400%2C335&ssl=1',
+                  width: 300,
+                  height: 300,
+                ),
+              ],
+            ),
           ),
-          Container(
-              child: const Center(
-            child: Text('Tuwaiq Academy', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-          )),
-          const SizedBox(
-            width: 10,
-            height: 10,
-          ),
-          Image.asset('asset/tuwaiq-academy-logo.png'),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'notifs'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'person'),
-        ],
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
+        ),
       ),
     );
   }
